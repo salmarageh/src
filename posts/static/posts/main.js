cf8b304c9eb12feeae233f128576a5d4d027b240
salmarageh <srageh5913@conestogac.on.ca>
2024-04-07 18:49:09 -0400

    coded to display post data using ajax call

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,16 +1,35 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
 
 
 $.ajax({
     type: 'GET',
-    url: '/hell-world/',
+    url: '/hello-world/',
     success: function(response){
         console.log('success', response.text)
         helloWorldBox.textContent = response.text
     },
     error: function(error){
         console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `
+        });
+    },
+    error: function(error){
+        console.log(error)
     }
 })
