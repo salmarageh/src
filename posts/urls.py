9c8469c1178bc4f7056bbce499a30e82283cd5d5
salmarageh <srageh5913@conestogac.on.ca>
2024-04-07 22:24:41 -0400

    Added buttons to detail page and stopped spinner

--- posts/urls.py
+++ posts/urls.py
@@ -4,13 +4,16 @@ from .views import (
     load_post_data_view,
     like_unlike_post,
     post_detail,
+    post_detail_data_view,
 )
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
-    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
+
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
 ]
