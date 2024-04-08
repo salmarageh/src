7a066b5b18071b159dc3dff514e62ac8ad64ff1e
salmarageh <srageh5913@conestogac.on.ca>
2024-04-07 21:53:28 -0400

    Added a detail post page

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
-    hello_world_view,
+    post_detail,
 )
 
 app_name = 'posts'
@@ -12,5 +12,5 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
-    path('hello-world/', hello_world_view, name='hello-world'),
+    path('<pk>/', post_detail, name='post-detail'),
 ]
